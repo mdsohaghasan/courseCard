bfc987fee3a85f0bdbb93aae2b6a9dccdffc8b8c
Sohag Hasan <mailsohaghasan@gmail.com>
2023-12-18 11:41:22 +0600

    card design

--- src/app/page.js
+++ src/app/page.js
@@ -5,27 +5,32 @@ import photo3 from "./img/course-3.png";
 
 export default function Home() {
   return (
-    <section className="container">
-      <div className="grid grid-cols-3	">
-        <div>
+    <section className="container mx-auto">
+      <div className="grid gap-4 grid-cols-3">
+        <div className="rounded-xl 	shadow-lg">
           <Image
-            className=""
+            className="rounded-t-xl"
             src={photo1}
             alt="course thumbnail"
             width={570}
             height={514}
           ></Image>
-          <h1>This is a one line title</h1>
-          <p>Two line paragraph Lorem ipsum dolor sit amet, consectetur</p>
-          <div>
-            <span>$1050</span>
-            <span>$750</span>
-            <span>You Save $250</span>
+          <div className="p-3">
+            <h1>This is a one line title</h1>
+            <p>Two line paragraph Lorem ipsum dolor sit amet, consectetur</p>
+            <div className="flex">
+              <span>$1050</span>
+              <span>$750</span>
+              <span>You Save $250</span>
+            </div>
           </div>
-          <div>
-            <span>10 hrs</span>
-            <span>308</span>
-            <span>17</span>
+          <hr className="border-x-gray-400"></hr>
+          <div className="p-3">
+            <div className="flex">
+              <span>10 hrs</span>
+              <span>308</span>
+              <span>17</span>
+            </div>
           </div>
         </div>
 
